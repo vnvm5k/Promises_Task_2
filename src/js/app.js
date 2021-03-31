09637a181ff4ce07794fcdd44ebda51efcd6cc9e
Vladimir Vorobtsov <vnvm5k@yandex.ru>
2021-03-31 23:11:32 +0300

    Update

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,24 @@
 import GameSavingLoader from './GameSavingLoader';
+import GameSaving from './GameSaving';
 
 (async () => {
   try {
     const response = GameSavingLoader.load();
-
-    console.log(response);
+    const {
+      id,
+      created,
+      userInfo: {
+        id: userId,
+        name: userName,
+        level: userLevel,
+        points: userPoints,
+      },
+    } = JSON.parse(response);
+    const gameSaving = new GameSaving(id, created, {
+      userId, userName, userLevel, userPoints,
+    });
+    return gameSaving;
   } catch (error) {
-    console.log(error);
+    throw new Error(error);
   }
 })();
